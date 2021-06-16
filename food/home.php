7bb32e2e89f734f72f618a6a26f3653b30e2cc5c
Mahmudul Hasan <40766222+MHElectronics@users.noreply.github.com>
2021-06-17 00:57:00 +0600

    Add files via upload

---
+++ food/home.php
@@ -0,0 +1,99 @@
+
+<!doctype html>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+      <title>WEDLOCK</title>
+    <style type="text/css">
+    body {
+      background-color: #F1E2E2;
+      /*font-family : arial;*/
+      font-size : 18px;
+      line-height : 18px;
+      color : #000;
+      padding: 4px;
+      margin: 12px;
+      background: url(h2.jpg) no-repeat;
+      margin-left : 21px;
+      background-size:  cover;
+    }
+    .btn1{
+        background-color: #003300;
+        border-radius: 15px;
+        color: white;
+      }
+  .btn1:hover {
+    background-color: #ff704d;
+    color: white;
+    }
+    .btn2{
+        line-height: 30px;
+        border-radius: 25px;
+        background-color: #ff704d;
+        color: white;
+      }
+.btn2:hover {
+  background-color: #004d00;
+  color: white;
+}
+ 
+div.container {
+text-align: center;
+ width: 400px;
+ 
+}
+
+h1{
+  color: white;
+}
+    <select name="Catagory" required style ="width:155px;height:47px;" >
+                  <option selected hidden value="">Catagory</option>
+                  <option value="Hall">Hall</option>
+                  <option value="Food">Food</option>
+                  <option value="Decor">Decor</option>
+                  <option value="Makeover">Makeover</option>
+                  <option value="Photo Blust">Photo Blust</option>
+                  <option value="Accessorise">Accessorise</option>
+                </select>
+  </style>
+    
+    </head>
+    <body>
+      <form action = "login.php" method = "post" >
+      
+      <!-- <div class="card"> -->
+         <div class="container">
+ <br>
+    <h1>WEDLOCK</h1>
+    <hr>
+    <ul class="list-group ">
+    <td><a href="../customer\new(customer).php" class="btn btn2" role="button">Sign Up As Customer </a></td>
+    <br>
+    <a href="../hall\new(hall).php" class="btn btn2" role="button">Sign Up As Hall Owner</a>
+    <br>
+    <a href="new(food).php" class="btn btn2" role="button">Sign Up As Food Company Owner</a>
+    <br>
+    <a href="../decor\new(decor).php" class="btn btn2" role="button">Sign Up As Decoration Company Owner</a>
+    <br>
+    <a href="../photoblust\new(photo).php" class="btn btn2" role="button">Sign Up As Photographer Company Owner</a>
+    <br><h>
+    <hr>
+    <a href="../index.html" class="btn btn1" role="button">Back</a>
+  </ul>
+     <!-- </div> -->
+</div>
+  </form>
+    
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ food/new(food).php
@@ -0,0 +1,146 @@
+<!doctype html>
+
+<?php
+	$servername = "localhost";
+	$username="root";
+	$password="";
+	$dbname="wedding";
+	try{
+		$conn = new mysqli($servername, $username,$password,$dbname);
+		}catch(MySQLi_Sql_Exception $ex){
+		echo("error in connection");
+		}
+	
+	if(isset($_POST['submit']))
+	{
+		$fcname=$_POST['fcname'];
+		$fcloc=$_POST['fcloc'];
+		$fcprice = $_POST['fcprice'];
+		$fcdes=$_POST['fcdes'];
+		$fcuser = $_POST['fcuser'];
+		$email = $_POST['email'];
+		$fcph = $_POST['fcph'];
+		$pass = $_POST['pass'];
+		$image = $_POST['image'];
+	
+  	$target = "image/".basename($image);
+	
+		
+		
+		$register_query = "INSERT INTO `food`(`fcname`,  `fcloc`,  `fcprice`, `fcdes`, `fcuser`, `email`, `fcph`, `pass`,`image`) VALUES ('$fcname','$fcloc','$fcprice', '$fcdes', '$fcuser', '$email','fcph','$pass','$image')";
+		
+		
+		$SELECT = "SELECT email From `food` where email = $email limit 1";
+
+		$result = $conn -> prepare($SELECT);
+
+		try{
+		
+			$register_result = mysqli_query($conn, $register_query);
+			if($register_result){
+				if(mysqli_affected_rows($conn)>0){
+				echo '<script>alert("registration successful")</script>';
+				}else{
+				echo '<script>alert("error in registration")</script>';
+				}
+		 
+				}
+			}catch(Exception $ex){
+			echo("error".$ex->getMessage());
+			}
+		}
+
+ 
+?>
+
+
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Registration Form</title>
+	
+	<meta name= "viewport" content="width = device - width, initial-scale = 1, shrink-to-fit = no">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+		<style type="text/css">
+			body {
+				
+				background: url(hh.jpg) fixed;
+				background-size: cover;
+			}
+		  	.btn{
+
+   			margin:10px ;
+    		line-height: 20px;
+			}
+div.card {
+	background-color:  #ffe6e6;
+	margin-left: 540px ;
+    width: 400px;
+    box-shadow: 0 10px 12px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+}
+
+div.container {
+  padding: 20px;
+ 
+}
+ul.list-group  {
+
+  margin-left: 30px;
+  margin-right: 30px;
+ 
+}
+
+		</style>
+	</head>
+	<br><br>
+	<body>
+
+		<form action = "" method = "post" >
+			
+			<div class="card">
+ <br>
+    <h1>WEDLOCK</h1>
+ 
+      <div class="container">
+      <ul class="list-group ">
+
+      	<input type = "text" name = "fcname" required = "required"placeholder="Company Name">
+    	<br>
+    	<input type = "text" name = "fcloc" required = "required"placeholder="Location">
+    	<br>
+    	<input type = "text" name = "fcprice" required = "required"placeholder="Price">
+    	<br>
+    	<input type = "text" name = "fcdes" required = "required"placeholder="Description">
+    	<br>
+    	<input type = "tel" name = "fcph" required = "required"placeholder="Phone">
+    	<br>
+    	<input type = "email" name = "email" required = "required"placeholder="Email">
+    	<br>
+    	<input type = "text" name = "fcuser" required = "required"placeholder="User Name">
+    	<br>
+    	<input type = "password" name = "pass" required = "required" placeholder="Password">
+    	<input type="file" name="image"  />
+
+    	
+  </ul>
+  <br>
+			<button type="submit" name="submit" value="SignUp" class="btn btn-primary" 
+			style="background-color:#660033">SignUp</button> 
+			<a href="home.php"  class="btn btn-info" role="button" style="background-color: #003300">Back</a>
+  </div>
+</div>
+	</form>
+	
+	<!-- Optional JavaScript -->
+	    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
